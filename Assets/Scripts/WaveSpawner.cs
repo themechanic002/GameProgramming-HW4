9bb1158a8e4762b978a1d3657ead7a0fea3d5987
abh <themechanic002@gmail.com>
2024-10-17 23:44:35 +0900

    Upload

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public float startTime;
+    public float endTime;
+    public float spawnRate;
+
+    void Start()
+    {
+        WavesManager.instance.AddWave(this);
+        InvokeRepeating("Spawn", startTime, spawnRate);
+        Invoke("EndSpawner", endTime);
+    }
+
+    void Spawn()
+    {
+        Instantiate(prefab, transform.position, transform.rotation);
+    }
+
+    void EndSpawner()
+    {
+        WavesManager.instance.RemoveWave(this);
+        CancelInvoke();
+    }
+}
